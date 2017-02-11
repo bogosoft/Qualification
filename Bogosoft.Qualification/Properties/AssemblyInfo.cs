ad5c2a88cfc848f3043ea7a80ea96213009a134e
Clayton Roth <croth0627@yahoo.com>
2017-02-11 11:46:48 -0800

    Build related updates.

--- Bogosoft.Qualification/Properties/AssemblyInfo.cs
+++ Bogosoft.Qualification/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bogosoft.Qualification")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A generic implementation of the specification pattern for .NET.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Clayton Roth")]
 [assembly: AssemblyProduct("Bogosoft.Qualification")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
