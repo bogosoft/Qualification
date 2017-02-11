09dba43b5b8d7e834c65c758b82ba971df00de7e
Clayton Roth <croth0627@yahoo.com>
2017-02-11 11:39:05 -0800

    Added extension method.

---
+++ Bogosoft.Qualification/EnumerableExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Bogosoft.Qualification
+{
+    /// <summary>
+    /// Extended functionality for types implementing <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Filter the current sequence for elements that match a given predicate.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the sequence.</typeparam>
+        /// <param name="source">The current sequence of elements.</param>
+        /// <param name="predicate">
+        /// A condition to filter the sequence by.
+        /// </param>
+        /// <returns>
+        /// A sequence of elements filtered by the given predicate.
+        /// </returns>
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> source, IQualify<T> predicate)
+        {
+            foreach(var x in source)
+            {
+                if (predicate.Qualify(x))
+                {
+                    yield return x;
+                }
+            }
+        }
+    }
+}
