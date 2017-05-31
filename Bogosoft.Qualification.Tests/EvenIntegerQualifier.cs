0923bc77e7f266641471c73ce4e7e932eedb8b54
Clayton Roth <croth0627@yahoo.com>
2017-05-30 21:15:53 -0700

    added unit test project

---
+++ Bogosoft.Qualification.Tests/EvenIntegerQualifier.cs
@@ -0,0 +1,10 @@
+﻿namespace Bogosoft.Qualification.Tests
+{
+    class EvenIntegerQualifier : IQualify<int>
+    {
+        public bool Qualify(int integer)
+        {
+            return integer % 2 == 0;
+        }
+    }
+}
---
+++ Bogosoft.Qualification.Tests/UnitTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Should;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bogosoft.Qualification.Tests
+{
+    [TestFixture, Category("Unit")]
+    public class UnitTests
+    {
+        [TestCase]
+        public void CanFilterSequenceByQualifier()
+        {
+            var length = 128;
+
+            var ints = Enumerable.Range(0, length);
+
+            ints.Where(new EvenIntegerQualifier()).Count().ShouldEqual(length / 2);
+        }
+    }
+}
